80b34211841975edfda9f66c01409fa2b9b15fd6
rodrigo-loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-02 14:19:02 -0300

    base spring boot

---
+++ src/main/java/br/com/controle360/api/Controle360Application.java
@@ -0,0 +1,13 @@
+package br.com.controle360.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controle360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controle360Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/controle360/api/Controle360ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controle360.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controle360ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
